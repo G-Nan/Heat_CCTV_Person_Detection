5a1f51fed08b8fe0f93fc4acd834712d02977fa3
Gnan <97678547+G-Nan@users.noreply.github.com>
2024-06-07 00:59:49 +0900

    UF13

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,23 @@
 import streamlit as st
-import cv2
-import numpy as np
-from ultralyticsplus import YOLO
-from PIL import Image
-
-# Load model
-model_path = 'thermal_detection.pt'
-model = YOLO(model_path)
-
-# Set model parameters
-model.overrides['conf'] = 0.25  # NMS confidence threshold
-model.overrides['iou'] = 0.45  # NMS IoU threshold
-model.overrides['agnostic_nms'] = False  # NMS class-agnostic
-model.overrides['max_det'] = 1000  # maximum number of detections per image
-
-def detect_objects(frame):
-    results = model(frame)
-    return results
-
-def draw_boxes(frame, results):
-    for result in results:
-        boxes = result.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
-            conf = box.conf[0]
-            cls = int(box.cls[0])
-            if cls in model.names:
-                label = model.names[cls]
-            else:
-                label = f'Unknown {cls}'
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    return frame
 
 def play_video(video_path):
-    cap = cv2.VideoCapture(video_path)
-    stframe = st.empty()
-    
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        # Perform object detection
-        results = detect_objects(frame)
-        
-        # Draw boxes on the frame
-        frame = draw_boxes(frame, results)
-        
-        # Convert BGR to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(frame)
-        
-        stframe.image(img, channels="RGB")
-    
-    cap.release()
+    video_file = open(video_path, 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
 
 def main():
     st.set_page_config(layout="wide")
-    st.title("Object Detection Using Normal/Infrared CCTV")
+    st.title("Normal/Infrared CCTV Videos")
 
     col1, col2 = st.columns(2)
 
     with col1:
         st.header("Normal CCTV")
-        play_video('video/CCTV_Detection_Real.mp4')
+        play_video('video/CCTV_Detection_Real_processed.mp4')
 
     with col2:
         st.header("Infrared CCTV")
-        play_video('video/CCTV_Detection_Thermal.mp4')
+        play_video('video/CCTV_Detection_Thermal_processed.mp4')
     
     st.markdown("""
     <div style="margin-top: 20px; padding: 10px; background-color: #f0f0f0; border: 1px solid #ccc;">
